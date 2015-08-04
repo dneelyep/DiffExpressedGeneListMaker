bfb695dfdb2418a606b821db1d2f1a4912d6bdd9
Daniel Neel <dneelyep@gmail.com>
2015-08-04 00:56:49 -0400

    Add the ability to specify a data directory, which houses CEL files, to the GUI tool. Update the project file as appropriate. Center the main form on the screen.

--- src/frmDiffExpressedGeneListMaker.java
+++ src/frmDiffExpressedGeneListMaker.java
@@ -1,14 +1,61 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class frmDiffExpressedGeneListMaker {
 
     private JPanel MainForm;
+    private JButton btnCreateList;
+    private JLabel lblChooseDataDirectory;
+    private JButton btnChooseDataDirectory;
+    private JTextField tfDataDirectory;
+
+    private GeneListParameters geneParameters;
+
+    public frmDiffExpressedGeneListMaker() {
+        // TODO Break this out into a separate handler method.
+        btnChooseDataDirectory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // TODO Clean up this nastiness.
+                // TODO This code should be moved into frmChooseDataDirectory's constructor, and then MainForm should be made private.
+                JFileChooser fc = new JFileChooser();
+                fc.setDialogTitle("Open data directory");
+                fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int returnVal = fc.showOpenDialog(MainForm);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File selectedFolder = fc.getSelectedFile();
+                    if (selectedFolder != null) {
+                        geneParameters = new GeneListParameters(selectedFolder);
+                        // TODO Setting the text as done here should be done automatically on property change.
+                        tfDataDirectory.setText(geneParameters.CELFileDirectory.toString());
+                    }
+                }
+/*
+                JFrame pickerFrame = new JFrame("Choose data directory");
+                pickerFrame.setContentPane(new frmChooseDataDirectory().MainForm);
+                pickerFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                pickerFrame.pack();
+
+                // Center the form on-screen.
+                pickerFrame.setLocationRelativeTo(null);
+
+                pickerFrame.setVisible(true);*/
+            }
+        });
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("frmDiffExpressedGeneListMaker");
+        JFrame frame = new JFrame("DEGLMaker (Differentially-expressed gene list maker)");
         frame.setContentPane(new frmDiffExpressedGeneListMaker().MainForm);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+
+        // Center the form on-screen.
+        frame.setLocationRelativeTo(null);
+
         frame.setVisible(true);
     }
 }
