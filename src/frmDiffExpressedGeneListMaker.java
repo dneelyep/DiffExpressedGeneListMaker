728a214c487779485d5676942842a254b43c8050
Daniel Neel <dneelyep@gmail.com>
2015-12-09 23:35:21 -0500

    Add code to display category types in the main window where samples are displayed.

--- src/frmDiffExpressedGeneListMaker.java
+++ src/frmDiffExpressedGeneListMaker.java
@@ -1,19 +1,28 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class frmDiffExpressedGeneListMaker {
 
+    //region Variables
     private JPanel MainForm;
     private JButton btnCreateList;
     private JLabel lblChooseDataDirectory;
     private JButton btnChooseDataDirectory;
     private JTextField tfDataDirectory;
+    private JTable tblDataCategories;
+    private JButton btnAddCategory;
 
-    private GeneListParameters geneParameters;
+    // TODO I shouldn't be allowed to pass in null data...
+    private GeneListParameters geneParameters = new GeneListParameters(null, new ArrayList<SampleCategory>());
+    //endregion
 
-
+    //region Methods
     public static void main(String[] args) {
         JFrame frame = new JFrame("DEGLMaker (Differentially-expressed gene list maker)");
         frame.setContentPane(new frmDiffExpressedGeneListMaker().MainForm);
@@ -28,6 +37,17 @@ public class frmDiffExpressedGeneListMaker {
 
     public frmDiffExpressedGeneListMaker() {
         initActionListeners();
+        initCategoryTable();
+    }
+
+    private void initCategoryTable() {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("CEL File");
+        TableColumn celFileColumn = new TableColumn(0);
+        celFileColumn.setHeaderValue("CEL File");
+
+
+        tblDataCategories.setModel(model);
     }
 
     private void initActionListeners() {
@@ -42,12 +62,45 @@ public class frmDiffExpressedGeneListMaker {
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     File selectedFolder = fc.getSelectedFile();
                     if (selectedFolder != null) {
-                        geneParameters = new GeneListParameters(selectedFolder);
+                        geneParameters.CELFileDirectory = selectedFolder;
                         // TODO Setting the text as done here should be done automatically on property change.
                         tfDataDirectory.setText(geneParameters.CELFileDirectory.toString());
+                        PopulateDataCategoryTable();
                     }
                 }
             }
         });
+
+        btnAddCategory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dlgAddCategory addCategoryDialog = new dlgAddCategory();
+
+                // TODO This code is super hacky. Clean it up.
+                if (addCategoryDialog.Display() == dlgAddCategory.DialogResult.OK) {
+                    // TODO Adding to this model should force the table to update, rather than me doing it manually here.
+                    geneParameters.SampleCategories.add(addCategoryDialog.ProvidedCategory);
+
+                    ((DefaultTableModel)tblDataCategories.getModel()).addColumn(addCategoryDialog.ProvidedCategory.Name);
+                    tblDataCategories.getColumnModel().getColumn(tblDataCategories.getColumnCount()-1).setCellEditor(new DefaultCellEditor(new JComboBox(addCategoryDialog.ProvidedCategory.ValidValues.toArray())));
+
+                    addCategoryDialog.dispose();
+                }
+            }
+        });
+
+        btnCreateList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GeneListMaker.MakeGeneList(geneParameters);
+            }
+        });
     }
+
+    private void PopulateDataCategoryTable() {
+        for (String celFile : geneParameters.CELFileNames()) {
+            ((DefaultTableModel) tblDataCategories.getModel()).addRow(new Object[] {celFile});
+        }
+    }
+    //endregion
 }
